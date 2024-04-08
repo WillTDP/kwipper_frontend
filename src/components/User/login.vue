e21b354c4170e1e1245c1cbbfee0df3a882ccba2
WillTDP <williamhazevoets@gmail.com>
2024-04-08 16:31:51 +0200

    Feat: all buttons for the login are added

--- src/components/User/login.vue
+++ src/components/User/login.vue
@@ -5,13 +5,37 @@
 <template>
   <div>
     <h1>Login</h1>
-    <form>
+    <form class="login">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
+      <a href="#">Wachtwoord vergeten?</a>
+      <form action="/action_page.php">
+        <input type="checkbox" id="remember" name="remember" value="remember">
+        <label for="remember">Remember me</label>
+      </form>
       <button type="submit">Login</button>
     </form>
   </div>
 </template>
 
 <style scoped>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+  }
+
+  .login {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    margin-bottom: 10px;
+  }
+
+  button {
+    margin-top: 10px;
+  }
 </style>
