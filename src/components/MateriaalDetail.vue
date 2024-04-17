4e679c641fe44c1dbb67e20091ec939d8e0271aa
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-04-18 00:47:54 +0200

    feat: start aan zoekertjes

---
+++ src/components/MateriaalDetail.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="page-wrap">
+        <div id="img-wrap">
+            <img v-bind:src="product.imageUrl">
+        </div>
+        <div id="product-details">
+            <h1>{{ product.name }}</h1>
+            <h3 id="price">€{{ product.price }} per dag</h3>
+            <p>Review score: {{ product.averageRating }}</p>
+            <button id="add-to-cart">Toevoegen aan winkelmandje</button>
+            <h4>Beschrijving</h4>
+            <p>{{ product.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { products } from '../fake-data.js';
+
+export default { 
+    name:'ProductDetailPage',
+    data() {
+        return {
+            product: products.find((p) => p.id === this.$route.params.id),
+        };
+    }
+};
+</script>
+
+<style scoped>
+  #page-wrap {
+    margin-top: 16px;
+    padding: 16px;
+    max-width: 600px;
+  }
+
+  #img-wrap {
+    text-align: center;
+  }
+
+  img {
+    width: 400px;
+  }
+
+  #product-details {
+    padding: 16px;
+    position: relative;
+  }
+
+  #add-to-cart {
+    width: 100%;
+  }
+
+  #price {
+    position: absolute;
+    top: 24px;
+    right: 16px;
+  }
+
+  .green-button {
+    background-color: green;
+  }
+
+  .grey-button {
+    background-color: #888;
+  }
+</style>
---
+++ src/fake-data.js
@@ -0,0 +1,72 @@
+import campImage1 from './assets/fouragetent.png';
+import campImage2 from './assets/gasbakken.png';
+import campImage3 from './assets/PVC-zeil.png';
+import campImage4 from './assets/quechua-rugtas.png';
+import campImage5 from './assets/quechua-tent.png';
+import campImage6 from './assets/seniorentent.png';
+import campImage7 from './assets/verlengkabel.png';
+import campImage8 from './assets/zeil.png';
+
+export const products = [{
+  id: '123',
+  name: 'fouragetent',
+  price: '30.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage1,
+  averageRating: '5.0',
+}, {
+  id: '234',
+  name: 'gasbakken',
+  price: '1.50',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage2,
+  averageRating: '5.0',
+}, {
+  id: '345',
+  name: 'PVC zeil',
+  price: '2.50',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage3,
+  averageRating: '5.0',
+}, {
+  id: '456',
+  name: 'quechua rugtas',
+  price: '6.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage4,
+  averageRating: '5.0',
+}, {
+  id: '567',
+  name: 'quechua tent',
+  price: '12.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage5,
+  averageRating: '5.0',
+}, {
+  id: '678',
+  name: 'seniorentent',
+  price: '25.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage6,
+  averageRating: '5.0',
+}, {
+  id: '789',
+  name: 'verlengkabel',
+  price: '0.50',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage7,
+  averageRating: '5.0',
+}, {
+  id: '890',
+  name: 'zeil',
+  price: '5.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: campImage8,
+  averageRating: '5.0',
+}];
+
+export const cartItems = [
+  products[0],
+  products[2],
+  products[3],
+];
