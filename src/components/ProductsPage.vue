a4e6f8eb23fc7d29c59e0e55a987745c494309fd
WillTDP <williamhazevoets@gmail.com>
2024-04-18 11:54:38 +0200

    Feat: premium check and component

--- src/components/ProductsPage.vue
+++ src/components/ProductsPage.vue
@@ -1,19 +1,22 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { products } from '../fake-data.js';
 import ProductItem from './ProductItem.vue';
+import ProductItemPremium from './ProductItemPremium.vue';
 
 const productsRef = ref(products);
+
+const nonPremiumProducts = computed(() => productsRef.value.filter(product => !product.premium));
+const premiumProducts = computed(() => productsRef.value.filter(product => product.premium));
 </script>
 
 <template>
-    <div id="page-wrap">
-        <div class="grid-wrap">
-          <ProductItem v-for="product in products" :key="product.id" :product="product" />
-        </div>
-    </div>
-    
-    
+  <div id="page-wrap">
+      <div class="grid-wrap">
+        <ProductItemPremium v-for="product in premiumProducts" :key="product.id" :product="product" />
+        <ProductItem v-for="product in nonPremiumProducts" :key="product.id" :product="product" />
+      </div>
+  </div>
 </template>
 
 <style scoped>
--- src/fake-data.js
+++ src/fake-data.js
@@ -14,6 +14,7 @@ export const products = [{
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
   imageUrl: campImage1,
   averageRating: '5.0',
+  premium: true,
 }, {
   id: '234',
   name: 'gasbakken',
