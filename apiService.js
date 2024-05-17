a714220da5d5d6d7427940c8353ff82b6696bce9
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-05-18 00:52:00 +0200

    Feat: Updated components and added api file

---
+++ apiService.js
@@ -0,0 +1,17 @@
+// src/apiService.js
+import axios from 'axios';
+
+// Zet de basis URL van de API
+const apiClient = axios.create({
+  baseURL: 'https://kwipper-back.onrender.com', // zet hier de webservice url
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default {
+  // Example function to fetch data from your API
+  fetchData() {
+    return apiClient.get('/api/v1/user');
+  },
+};
--- src/components/Materiaal/ProductsPage.vue
+++ src/components/Materiaal/ProductsPage.vue
@@ -4,6 +4,8 @@ import { products } from '../../fake-data.js'
 import ProductItem from './ProductItem.vue';
 import ProductItemPremium from './ProductItemPremium.vue';
 import filtermenuMobile from './filtermenu-mobile.vue';
+import filtermenuDesktop from './filtermenu-desktop.vue';
+import apiseervice from '/apiService.js';
 
 let selectedCategory = ref(null);
 
@@ -20,24 +22,54 @@ const premiumProducts = computed(() => {
 });
 const state = reactive({
   mobile: window.innerWidth < 811, // Initialize mobile state
+  desktop: window.innerWidth > 811 // Initialize desktop state
 });
 
 function checkMobile() {
   state.mobile = window.innerWidth < 811;
 }
 
+function checkDesktop() {
+  state.desktop = window.innerWidth > 811;
+}
+
 onMounted(() => {
   window.addEventListener('resize', checkMobile);
+  window.addEventListener('resize', checkDesktop);
 });
 
 onUnmounted(() => {
   window.removeEventListener('resize', checkMobile);
+  window.removeEventListener('resize', checkDesktop);
 });
+
+export const dataMixin = {
+  data() {
+    return {
+      data: null,
+    };
+  },
+  created() {
+    // Fetch data when the component is created
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await apiService.fetchData();
+        this.data = response.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+};
 </script>
 
 <template>
   <div id="page-wrap">
       <filtermenuMobile @filter="filterProducts" v-if="state.mobile" />
+      <filtermenuDesktop @filter="filterProducts" v-if="state.desktop" />
       <div class="grid-wrap">
         <ProductItemPremium v-for="product in premiumProducts" :key="product.id" :product="product" />
         <ProductItem v-for="product in nonPremiumProducts" :key="product.id" :product="product" />
@@ -58,5 +90,6 @@ onUnmounted(() => {
     justify-content: space-between;
     margin-top: 16px;
     float: none;
+    
   }
 </style>
