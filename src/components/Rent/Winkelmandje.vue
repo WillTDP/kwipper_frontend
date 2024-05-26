93ae09e02f036750dee78f9c4d7f317ff54b3c0e
WillTDP <williamhazevoets@gmail.com>
2024-05-26 16:39:49 +0200

    Feat: replaced mock data with fake data for the but wait segment filtering on an email address from that api

--- src/components/Rent/Winkelmandje.vue
+++ src/components/Rent/Winkelmandje.vue
@@ -1,4 +1,14 @@
 <script setup>
+import ProductItem from '../Materiaal/ProductItem.vue';
+import { computed } from 'vue';
+import { products } from '../../fake-data.js';
+
+const sellerEmail = 'chirokuringen@gmail.com'; // replace with the actual seller's email
+
+const sameSellerProducts = computed(() => {
+  return products.filter(product => product.seller.email === sellerEmail);
+});
+
 
 </script>
 
@@ -16,7 +26,7 @@
                                 <button class="remove">Verwijderen</button>
                             </div>
                             <div class="item-amount">
-                            <select class="input-limited" v-model="selectedPriceValue">
+                            <select class="input-limited">
                                 <option value="0-5">1</option>
                                 <option value="5-10">2</option>
                                 <option value="10-20">3</option>
@@ -36,7 +46,7 @@
                                 <button class="remove">Verwijderen</button>
                             </div>
                             <div class="item-amount">
-                                <select class="input-limited" v-model="selectedPriceValue">
+                                <select class="input-limited">
                                 <option value="0-5">1</option>
                                 <option value="5-10">2</option>
                                 <option value="10-20">3</option>
@@ -51,16 +61,9 @@
                 </div>
                 <div class="but_wait">
                     <p>Wacht! Deze gebruiker verkoopt ook:</p>
-                    <div class="items">
-                        <div class="item">
-                            <img src="../../assets/fouragetent.png" alt="placeholder" />
-                            <div class="item-info">
-                                <div class="name">
-                                    <p>Productnaam</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div class="productitem">
+                        <ProductItem v-for="product in sameSellerProducts" :key="product.id" :product="product"/>
+                    </div>     
                 </div>
             </div>
             <div class="overzicht">
@@ -109,6 +112,9 @@
 
     h1 {
         color: #2b5740;
+        margin: 0;
+        padding: 0;
+        padding-bottom: 1em;
     }
 
 
@@ -209,6 +215,24 @@
         border: none;
         border-radius: 9px;
         padding: 0.5em;
+    }
+
+    .but_wait {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+
+    .but_wait p {
+        font-size: 16px;
+        font-weight: 700;
+    }
+
+    .productitem {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        background-color: #f0f2f1;
     }
 
     @media screen and (max-width: 800px){
