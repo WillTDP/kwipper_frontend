a3d9b2b247945beb6d8756882f642b37965dbd1b
zghr <r0878962@student.thomasmore.be>
2024-06-08 15:19:47 +0200

    Chore: add more filters

--- src/components/Materiaal/ProductPage/Parts/categorymenu-desktop.vue
+++ src/components/Materiaal/ProductPage/Parts/categorymenu-desktop.vue
@@ -36,10 +36,10 @@ const filter = (category) => {
 
     <div class="segment">
       <p class="title">Legermateriaal</p>
-      <p :class="{'link': true, 'selected': selectedCategory === 'Legertenten'}" 
-        @click="selectedCategory !== 'Legertenten' && filter('Legertenten')">
+      <p :class="{'link': true, 'selected': selectedCategory === 'legertenten'}" 
+        @click="selectedCategory !== 'legertenten' && filter('legertenten')">
         Legertenten
-        <span v-if="selectedCategory === 'Legertenten'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
+        <span v-if="selectedCategory === 'legertenten'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
       </p>
       <p :class="{'link': true, 'selected': selectedCategory === 'seniorentent'}" 
         @click="selectedCategory !== 'seniorentent' && filter('seniorentent')">
@@ -112,6 +112,35 @@ const filter = (category) => {
             @click="selectedCategory !== 'houweel' && filter('houweel')">
             Houweel
             <span v-if="selectedCategory === 'houweel'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
+          </p>
+        </div>
+
+    <div class="segment">
+          <p class="title">Outdoor activiteiten</p>
+          <p :class="{'link': true, 'selected': selectedCategory === 'kajak'}" 
+            @click="selectedCategory !== 'kajak' && filter('kajak')">
+            Kajak
+            <span v-if="selectedCategory === 'kajak'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
+          </p>
+          <p :class="{'link': true, 'selected': selectedCategory === 'kano'}" 
+            @click="selectedCategory !== 'kano' && filter('kano')">
+            Kano
+            <span v-if="selectedCategory === 'kano'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
+          </p>
+          <p :class="{'link': true, 'selected': selectedCategory === 'fiets'}" 
+            @click="selectedCategory !== 'fiets' && filter('fiets')">
+            Fiets
+            <span v-if="selectedCategory === 'fiets'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
+          </p>
+          <p :class="{'link': true, 'selected': selectedCategory === 'klimuitrusting'}" 
+            @click="selectedCategory !== 'klimuitrusting' && filter('klimuitrusting')">
+            Klimuitrusting
+            <span v-if="selectedCategory === 'klimuitrusting'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
+          </p>
+          <p :class="{'link': true, 'selected': selectedCategory === 'Kampvuurbenodigdheden'}" 
+            @click="selectedCategory !== 'Kampvuurbenodigdheden' && filter('Kampvuurbenodigdheden')">
+            Kampvuurbenodigdheden
+            <span v-if="selectedCategory === 'Kampvuurbenodigdheden'" @click.stop="selectedCategory = null; emit('filter', null)" class="x">x</span>
           </p>
         </div>
 
